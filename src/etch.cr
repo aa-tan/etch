6596021425f53f536439a3ee28fb2acb9332af25
aa-tan <a.tan@keio.jp>
2018-11-28 15:21:04 +0900

    Added handling for building json object

--- src/etch.cr
+++ src/etch.cr
@@ -1,6 +1,15 @@
 # TODO: Write documentation for `Etch`
+require "json"
 module Etch
   VERSION = "0.1.0"
+  path = ENV["HOME"]+"/etchfile.json"
+  data = {"etchpath" => path,"outpath" => "/usr/local/bin/"}
+      result = JSON.build do |json|
+	      json.object do
+  	      json.field "etchfile", data["etchpath"]
+          json.field "outpath", data["outpath"]
+	      end
+      end
   
   path = "etchfile" 
   abort "Missing etchfile", 1 if !File.file? path
