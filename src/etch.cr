e65e40a7e21af91f6cbaaa8283e7b42e6e1e6df4
aa-tan <a.tan@keio.jp>
2018-11-28 15:38:16 +0900

    Added command parser function

--- src/etch.cr
+++ src/etch.cr
@@ -34,6 +34,17 @@ module Etch
     puts converted
   end
 
+  def run_cmd(cmd, args)
+    stdout = IO::Memory.new
+    stderr = IO::Memory.new
+    status = Process.run(cmd, args: args, output: stdout, error: stderr)
+    if status.success?
+      {status.exit_code, stdout.to_s}
+    else
+      {status.exit_code, stderr.to_s}
+    end
+    puts "stdout:\n#{stdout}"
+    puts "stderr:\n#{stderr}"
   end
   
   path = "etchfile" 
