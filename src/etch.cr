e5892aa7b724498a9c9fc9c94a54c9d83d872867
aa-tan <a.tan@keio.jp>
2018-11-28 15:24:38 +0900

    Logic for missing etchfile

--- src/etch.cr
+++ src/etch.cr
@@ -4,12 +4,30 @@ module Etch
   VERSION = "0.1.0"
   path = ENV["HOME"]+"/etchfile.json"
   data = {"etchpath" => path,"outpath" => "/usr/local/bin/"}
+  if !File.file? data["etchpath"]
+    puts "Etchfile missing, setting up..."
+    print "Enter path to save crystal application binaries (Default is /usr/local/bin/): "
+    input = gets
+    if input.nil?
+      puts "Invalid input"
+    elsif input != ""
+      data["outpath"] = input.as(String).chomp
+    end
+    if File.directory? data["outpath"]
+      etchfile = File.open data["etchpath"],"w"
       result = JSON.build do |json|
 	      json.object do
   	      json.field "etchfile", data["etchpath"]
           json.field "outpath", data["outpath"]
 	      end
       end
+      etchfile.print result
+      etchfile.close
+    else
+      puts "Invalid path"
+    end
+
+  end
   
   path = "etchfile" 
   abort "Missing etchfile", 1 if !File.file? path
